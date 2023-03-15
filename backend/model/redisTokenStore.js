eba63df29b2d133ef647c5e952534f85a7d0aa30
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-03-15 15:27:53 +0700

    Delete backend directory

--- backend/model/redisTokenStore.js
+++
@@ -1,3 +0,0 @@
-let redisTokenStore = [];
-
-module.exports = { redisTokenStore };
--- backend/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controller/authController");
-
-router.post("/register", authController.registerHandler);
-router.post("/login", authController.loginHandler);
-router.post("/logout", authController.logoutHandler);
-router.post("/token", authController.tokenHandler);
-
-module.exports = router;
